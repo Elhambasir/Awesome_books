7f0e7818578d03ffa91930ecad183fe9cdd18708
Abdurahim Miftah <abdugreat3@gmail.com>
2023-03-21 16:43:05 +0200

    Added removeBook function in class

--- script.js
+++ script.js
@@ -1,58 +1,7 @@
-
-// const title = document.querySelector('.bookTitle');
-// const author = document.querySelector('.author');
-// const add = document.querySelector('.btnAdd');
-
-// add.onclick = function storeData() {
-//   const bookInfo =
-//   {
-//     title: title.value,
-//     author: author.value,
-//   };
-//   let rd = JSON.parse(localStorage.getItem('bookInfo'));
-//   if (rd) {
-//     rd.push(bookInfo);
-//   } else {
-//     rd = [];
-//     rd.push(bookInfo);
-//   }
-//   const string = JSON.stringify(rd);
-//   localStorage.setItem('bookInfo', string);
-// };
-
-// window.onload = function retrieveData() {
-//   const data = JSON.parse(localStorage.getItem('bookInfo'));
-//   const table = document.querySelector('.table');
-//   data.forEach(element => {
-//     const generatedContent = `
-//     <tr>
-//     <td>${element.title}</td>
-//     <td>${element.author}</td>
-//     <td><button id="removeBtn" onclick='removeItem("${element.title}")'>Remove</button></td>
-//     <td><hr></td>
-//   </tr>
-//     `;
-//     table.innerHTML += generatedContent;
-//   });
-// };
-
-// const removeButton = document.querySelector('#removeBtn');
-// removeButton.addEventListener('click', () => {
-//   removeItem("id");
-// });
-// function removeItem(item) {
-//   let bookList = JSON.parse(localStorage.getItem('bookInfo'));
-//   bookList = bookList.filter((element) => {
-//     return item != element.title;
-//   });
-//   const string = JSON.stringify(bookList);
-//   localStorage.setItem('bookInfo', string);
-//   window.location.reload();
-// }
 const titleInput = document.querySelector('.bookTitle');
 const authorInput = document.querySelector('.author');
 const addBtn = document.querySelector('.btnAdd');
-
+const removeButton = document.querySelector('#removeBtn');
 
 class Books {
   constructor() {
@@ -75,45 +24,51 @@ class Books {
     const booksString = JSON.stringify(rd);
     localStorage.setItem('bookInfo', booksString);
     this.retrieveData();
-////////////////////////
-    // const bookInfo =
-    // {
-    //   title: title.value,
-    //   author: author.value,
-    // };
-    // let rd = JSON.parse(localStorage.getItem('bookInfo'));
-    // if (rd) {
-    //   rd.push(bookInfo);
-    // } else {
-    //   rd = [];
-    //   rd.push(bookInfo);
-    // }
-    // const string = JSON.stringify(rd);
-    // localStorage.setItem('bookInfo', string);
   };
+
+  removeItem(item) {
+    console.log('hello')
+    let bookList = JSON.parse(localStorage.getItem('bookInfo'));
+    bookList = bookList.filter((element) => {
+      return item != element.title;
+    });
+    const string = JSON.stringify(bookList);
+    localStorage.setItem('bookInfo', string);
+    window.location.reload();
+  }
 
   retrieveData() {
     const data = JSON.parse(localStorage.getItem('bookInfo'));
-    const table = document.querySelector('.table');
-    let bookData = '';
-    data.forEach(element => {
-      const generatedContent = `
-      <tr>
-      <td>${element.title}</td>
-      <td>${element.author}</td>
-      <td><button id="removeBtn" onclick='removeItem("${element.title}")'>Remove</button></td>
-      <td><hr></td>
-    </tr>
-      `;
-      bookData+= generatedContent;
-    });
-    table.innerHTML = bookData;
+    if (data) {
+      const table = document.querySelector('.table');
+      let bookData = '';
+      data.forEach(element => {
+        const generatedContent = `
+        <tr>
+        <td>${element.title}</td>
+        <td>${element.author}</td>
+        <td><button id="removeBtn" onclick='removeFun("${element.title}")'>Remove</button></td>
+        <td><hr></td>
+      </tr>
+        `;
+        bookData += generatedContent;
+      });
+      table.innerHTML = bookData;
+    }
   };
 }
 
 const obj = new Books();
-window.onload = obj.retrieveData();
+window.location.reload = obj.retrieveData();
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   obj.addBooks();
 })
+
+function removeFun(item){
+  obj.removeItem(item)
+};
+
+removeButton.addEventListener('click', () => {
+  removeItem("id");
+});
